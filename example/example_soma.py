92521e97933a9c68bb588f521926a36cea05230c
SophiaShanyf <shanyufei2001@gmail.com>
2024-12-03 22:59:10 -0800

    test

--- example/example_soma.py
+++ example/example_soma.py
@@ -1,8 +1,4 @@
 from neuron_simulator import run_simulation
 
 run_simulation('soma')
-'''
-import sys
-
-print(sys.path)
-sys.path.append('/Users/sooophias/Downloads/neuron_simulator/537_11/lib/python3.11/site-packages')'''
+run_simulation('dend_soma')
